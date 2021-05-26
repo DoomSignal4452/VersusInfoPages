1f85378426462a726776b34cf478693e423726f0
DoomSignal4452 <76997074+DoomSignal4452@users.noreply.github.com>
2021-05-26 14:24:31 +0200

    initial files added

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// ###############################################################
+// ##                          Model                            ##
+// ###############################################################
+
+const Monster = [
+    { id: 01, Species: 'Dog(WIP)',      Health: 120, MagicPoints: 80,  AttackMin: 5, AttackMax: 10, CritChance: 30, DodgeRate: 10, Element: 'Neutral',  Affinity: 'Normal',     Picture: "picWIP.png"},
+    { id: 02, Species: 'Fox(WIP)',      Health: 100, MagicPoints: 80,  AttackMin: 8, AttackMax: 15, CritChance: 40, DodgeRate: 15, Element: 'Wind',     Affinity: 'Plant',      Picture: "picWIP.png"},
+    { id: 03, Species: 'Purryl(WIP)',   Health: 150, MagicPoints: 50,  AttackMin: 2, AttackMax: 6,  CritChance: 20, DodgeRate: 10, Element: 'Stone',    Affinity: 'Cold',       Picture: "picWIP.png"},
+    { id: 04, Species: 'Gecko(WIP)',    Health: 90,  MagicPoints: 90,  AttackMin: 5, AttackMax: 8,  CritChance: 35, DodgeRate: 25, Element: 'Fire',     Affinity: 'Inorganic',  Picture: "picWIP.png"},
+    { id: 05, Species: 'Dinosaur(WIP)', Health: 110, MagicPoints: 70,  AttackMin: 3, AttackMax: 6,  CritChance: 20, DodgeRate: 20, Element: 'Water',    Affinity: 'Normal',     Picture: "picWIP.png"},
+    { id: 06, Species: 'Serpent(WIP)',  Health: 160, MagicPoints: 90,  AttackMin: 2, AttackMax: 3,  CritChance: 50, DodgeRate: 5,  Element: 'Wind',     Affinity: 'Cold',       Picture: "picWIP.png"},
+    { id: 07, Species: 'Horse(WIP)',    Health: 100, MagicPoints: 100, AttackMin: 8, AttackMax: 10, CritChance: 5,  DodgeRate: 10, Element: 'Stone',    Affinity: 'Plant',      Picture: "picWIP.png"},
+    { id: 08, Species: 'Meercat(WIP)',  Health: 130, MagicPoints: 50,  AttackMin: 4, AttackMax: 6,  CritChance: 60, DodgeRate: 15, Element: 'Fire',     Affinity: 'Normal',     Picture: "picWIP.png"},
+    { id: 09, Species: 'Bird(WIP)',     Health: 140, MagicPoints: 40,  AttackMin: 1, AttackMax: 10, CritChance: 20, DodgeRate: 0,  Element: 'Water',    Affinity: 'Inorganic',  Picture: "picWIP.png"},
+
+];
+console.table(Monster);
+
+
+
+// ###############################################################
+// ##                          View                             ##
+// ############################################################### <div></div>
+
+updateView();
+function updateView() { 
+    document.getElementById('app').innerHTML = `
+
+    `;
+}
+
+
+// ###############################################################
+// ##                       Controller                          ##
+// ############################################################### <div></div>
+
