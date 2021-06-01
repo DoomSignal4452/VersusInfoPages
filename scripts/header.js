9ee4d527327519c970669b7dfe21c1dd78103e6f
DoomSignal4452 <76997074+DoomSignal4452@users.noreply.github.com>
2021-06-01 14:10:51 +0200

    added header file

---
+++ scripts/header.js
@@ -0,0 +1,35 @@
+showHeader()
+
+function showHeader() {
+    document.getElementById('header').innerHTML = `
+    <div class=headerWrap>
+
+        <div class=headerTop>I is Header</div>
+
+        <div class=headerBot>
+
+            <div class="navBar">
+                <div class="hoverRed"><a href="index.html">Home</a></div>
+
+                <div class="hoverRed"><a href="index.html">Monster List</a></div>
+
+                <div class="hoverRed"><a href="index.html">Elements & Affinity</a></div>
+
+                <div class="hoverRed"><a href="index.html">Ghosts: Special Monsters</a></div>
+
+                <div class="hoverRed"><a href="index.html">Team Builder</a></div>
+
+                <div class="hoverRed floatRight"><a href="index.html">About</a></div>
+
+            </div>
+
+        </div>
+
+    </div>
+    `;
+}
+
+
+
+
+// <div></div>
