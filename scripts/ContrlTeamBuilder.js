5db0570193993ed1c92c7c8dfc2fd42f006c8622
DoomSignal4452 <76997074+DoomSignal4452@users.noreply.github.com>
2021-06-15 15:45:09 +0200

    endofday
    added UnitTests

--- scripts/ContrlTeamBuilder.js
+++ scripts/ContrlTeamBuilder.js
@@ -1,51 +1,39 @@
 
-
 function BuildMonsterList() { // Add element and affinity pics to show in list.
     let ML="";
     let counter=0;
     for(var i = 0;i<Monster.length;i++) {
-        // console.log(Monster[i])
-        
         ML+=
         `<div id="Monster${Monster[i].id}" class="TBMonsterList ${model.tempStyles.TeamBuilder.ListSelection} ${isIselectedList(counter)}" onclick="SelectFromList(this.id)">
         ${Monster[i].Species}
         </div>`
-        
-        // $.('#myList').append("<option>"+Monster[i]+"</option>");
+
         counter++;
-    }
+    };
     return ML;
-}
+};
 
 function SelectFromList(DivId) {
     let OnlyNum = DivId.replace('Monster', '') // Removes 'Monster' from DivId
     let MonId = parseFloat(OnlyNum); // Makes string into mumber
-
-    //console.log('monster id is '+ MonId);
-    //let index = Monster.findIndex(MonId)
-
 
     let index = Monster.findIndex( element => {
         if (element.id === MonId) {
           return true;
         }
     });
-    //console.log('index is '+ index);
 
-    //console.log(index);
-    //console.log(model.teamMaker.marker.list)
     model.teamMaker.select.list = Monster[index];
     model.teamMaker.marker.list = index;
-    //console.log(model.teamMaker.marker.list)
     ViewTeamBuild();
-}
+};
 
 function AddToTeam() {
     if (model.teamMaker.team.length < 3 && model.teamMaker.select.list.Species != undefined){ // checks to ensure there is never more than 3 monsters in team & that selection is not empty(undefined)
         model.teamMaker.team.push(model.teamMaker.select.list);
         ViewTeamBuild();
-    }
-}
+    };
+};
 
 function BuildTeamList() { // Add element and affinity pics to show in list.
     let ML=``;
@@ -60,7 +48,7 @@ function BuildTeamList() { // Add element and affinity pics to show in list.
         counter++;
     }
     return ML;
-}
+};
 
 function SelectFromTeam(DivId) {    
     let OnlyNum = DivId.replace('Team', '') // Removes 'Team' from DivId
@@ -69,31 +57,32 @@ function SelectFromTeam(DivId) {
     model.teamMaker.select.team = model.teamMaker.team[index];
     model.teamMaker.marker.team = index;
 
-    //console.log(model.teamMaker.team[index]);
     ViewTeamBuild();
-}
+};
 
 function isIselected(counter) {
-    //console.log('counter Team '+counter)
-    if(counter == model.teamMaker.marker.team) {return 'isSelected'}
+    if (typeof model.teamMaker.marker.team != 'number') {return '';}
+
+    else if(counter === model.teamMaker.marker.team) {return 'isSelected';}
+
     else {return ''};
-}
+};
 
 function isIselectedList(counter) {
-    //console.log('counter List '+counter)
-    if(counter == model.teamMaker.marker.list) {return 'isSelected'}
+    if (typeof model.teamMaker.marker.team != 'number') {return '';}
+
+    else if(counter === model.teamMaker.marker.list) {return 'isSelected';}
+
     else {return ''};
-}
+};
 
 function removeFromTeam() {
-
-    if (model.teamMaker.marker.team == undefined) {
-        return
+    if (typeof model.teamMaker.marker.team != 'number' || model.teamMaker.marker.team < 0 || model.teamMaker.marker.team > 3) {
+        return false;
     } else {
         model.teamMaker.team.splice(model.teamMaker.marker.team, 1);
         model.teamMaker.marker.team = undefined;
+        ViewTeamBuild();
+        return true;
     };
-
-    //console.log(index);
-    ViewTeamBuild();
-}
+};
