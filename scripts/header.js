cb37f4328ad6c50c1b4f36858352fed6f7d0a65d
DoomSignal4452 <76997074+DoomSignal4452@users.noreply.github.com>
2021-06-16 14:13:46 +0200

    fix maybe

--- scripts/header.js
+++ scripts/header.js
@@ -11,13 +11,13 @@ function ViewHeader() {
             <div class="navBar">
                 <div class="hoverRed"><a href="index.html">Home</a></div>
 
-                <div class="hoverRed"><a href="/pages/MonsterList.html">Monster List</a></div>
+                <div class="hoverRed"><a href="pages/MonsterList.html">Monster List</a></div>
 
                 <div class="hoverRed"><a href="#">Elements & Affinity</a></div>
 
                 <div class="hoverRed"><a href="#">Ghosts: Special Monsters</a></div>
 
-                <div class="hoverRed"><a href="/pages/TeamBuilder.html">Team Builder</a></div>
+                <div class="hoverRed"><a href="pages/TeamBuilder.html">Team Builder</a></div>
 
                 <div class="hoverRed floatRight"><a href="#">About</a></div>
 
