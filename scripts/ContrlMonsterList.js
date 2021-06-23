2de11b55f9ae6b014220ec01a29e15547ce4bd54
DoomSignal4452 <76997074+DoomSignal4452@users.noreply.github.com>
2021-06-23 15:14:42 +0200

    endofday

--- scripts/ContrlMonsterList.js
+++ scripts/ContrlMonsterList.js
@@ -1,10 +1,13 @@
 
-function BuildBigMonsterList() { // Add element and affinity pics to show in list.
+function BuildBigMonsterList2() { // Add element and affinity pics to show in list.
     let ML="";
     let counter=0;
     for(var i = 0;i<Monster.length;i++) {
         ML+=
-        `<div id="Monster${Monster[i].id}" class="TBMonsterList" onclick="SelectFromList(this.id)">
+        `<div id="Monster${Monster[i].id}" class="TBMonsterList BigMonsterList">
+
+            <div class="BM_small_pic"><img src="../img/${Monster[i].Picture}" alt="Picture of ${Monster[i].Species}" class="BM_small_pic"></div>
+
             <div><h3>${Monster[i].Species}</h3></div>
 
             <div>
@@ -13,6 +16,26 @@ function BuildBigMonsterList() { // Add element and affinity pics to show in lis
             </div>
         
         </div>`
+
+        counter++;
+    };
+    return ML;
+};
+
+
+
+
+function BuildBigMonsterList() { // Add element and affinity pics to show in list.
+    let ML="";
+    let counter=0;
+    for(var i = 0;i<Monster.length;i++) {
+        ML+=
+        `<tr>
+          <td class="slimTD"><img src="../img/${Monster[i].Picture}" alt="Picture of ${Monster[i].Species}" class="BM_small_pic"></td>
+          <td class="PadLeft">${Monster[i].Species}</td>
+          <td class="PadLeft">${Monster[i].Element}</td>
+          <td class="PadLeft">${Monster[i].Affinity}</td>
+        </tr>`
 
         counter++;
     };
--- scripts/ViewMonsterList.js
+++ scripts/ViewMonsterList.js
@@ -1,10 +1,29 @@
 
-function ViewMonsterList() {
+function ViewMonsterList2() {
     document.getElementById('app').innerHTML = `
         <div class="spacer"></div>
 
         <div class="infoBox infoBoxWide">
             ${BuildBigMonsterList()}
+        </div>
+    `;
+}
+
+
+function ViewMonsterList() {
+    document.getElementById('app').innerHTML = `
+        <div class="spacer"></div>
+
+        <div class="infoBox infoBoxWide BigMonsterList">
+            <table style="width:100%" class="">
+                <tr>
+                    <th>Picture</th>
+                    <th>Species</th>
+                    <th>Element</th>
+                    <th>Affinity</th>
+                </tr>
+                ${BuildBigMonsterList()}
+            </table>
         </div>
     `;
 }
